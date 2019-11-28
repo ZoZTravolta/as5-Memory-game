a9f80f9b8db51cc46953e42f95f59439fdba05d2
Zohar <Zoharsaghi@gmail.com>
2019-11-28 19:42:58 +0200

    filliped working all is good

--- js/script.js
+++ js/script.js
@@ -1,15 +1,21 @@
 'use strict';
 
+$(document).ready(function(){
+    game.start();
+})
 const imgBase = {};
 const cards = {};
 const game = {};
 
+
+
 game.start=()=>{
     imgBase.imgApi = 'https://dog.ceo/api/breeds/image/random';
-    imgBase.numOfImagesToGet = 2;
+    imgBase.numOfImagesToGet = 9;
     imgBase.imgArr = [];
     imgBase.buildPicsDataBase();
     cards.reset();
+    game.score = 0;
 }
 
 
@@ -29,12 +35,21 @@ imgBase.buildPicsDataBase = async () => {
         }
     }
     imgBase.shuffleArray();
+    imgBase.loadAllImages();
     $('.loader').removeClass('show');
 
 }
+
+imgBase.loadAllImages =() =>{
+    for(let img of imgBase.imgArr){
+        $('.img-tank').append(`<img src="${img}" />`)
+    }
+    
+}
+
 imgBase.shuffleArray = () => {
-    function shuffle(arra1) {
-        let ctr = arra1.length;
+    function shuffle(arr) {
+        let ctr = arr.length;
         let temp;
         let index;
         // While there are elements in the array
@@ -44,12 +59,14 @@ imgBase.shuffleArray = () => {
             // Decrease ctr by 1
             ctr--;
             // And swap the last element with it
-            temp = arra1[ctr];
-            arra1[ctr] = arra1[index];
-            arra1[index] = temp;
+            temp = arr[ctr];
+            arr[ctr] = arr[index];
+            arr[index] = temp;
         }
-        return arra1;
+        return arr;
     }
+    shuffle(imgBase.imgArr)
+    console.log(imgBase.imgArr)
     cards.createCards();
 }
 
@@ -61,10 +78,12 @@ cards.reset=()=>{
 
 cards.createCards = () => {
     for (let index in imgBase.imgArr) {
-        $('.game').append(`<div class="card" id="${index}" value="${index}"  />`);
+        $('.game').append(`<div class="card" id="${index}" value="${index}">
+            <div class="front"></div>
+            <div class="back"></div>
+        </div>`);
         $(`#${index}`).click(function(){
-            cards.clickOnCard(this);
-            
+            cards.clickOnCard(this);   
         })
     }
 };
@@ -72,7 +91,7 @@ cards.createCards = () => {
 cards.clickOnCard = (cardThatClicked) => {
     if (typeof $(cardThatClicked).attr("disabled") === typeof undefined || $(cardThatClicked).attr("disabled") === false) {
         $(cardThatClicked).attr("disabled", true);
-        cards.checkIfTwoFilliped(cardThatClicked);
+        cards.flipCard(cardThatClicked);
     }
     else{
         console.log('disabled')
@@ -80,38 +99,53 @@ cards.clickOnCard = (cardThatClicked) => {
     
 }
 
-cards.checkIfTwoFilliped = (cardThatClicked) =>{
+cards.flipCard = (cardThatClicked) =>{
+    const back = cardThatClicked.getElementsByClassName('back')[0];
     if (cards.card1 == null){
         cards.card1 = cardThatClicked;
-        cardThatClicked.style.backgroundImage = `url("${imgBase.imgArr[cardThatClicked.id]}")`;
+        $(cardThatClicked).addClass('is-flipped');
+        back.style.backgroundImage = `url("${imgBase.imgArr[cardThatClicked.id]}")`;
     }
     else if (cards.card2 == null){
         cards.card2 = cardThatClicked;
-        cardThatClicked.style.backgroundImage = `url("${imgBase.imgArr[cardThatClicked.id]}")`;
+        $(cardThatClicked).addClass('is-flipped');
+        back.style.backgroundImage = `url("${imgBase.imgArr[cardThatClicked.id]}")`;
         cards.checkIfMatch()
     }
 }
 
 cards.checkIfMatch =()=>{
-    if ($(cards.card1).css('background-image') == $(cards.card2).css('background-image')){
-        console.log('giveScore();');
-        $(cards.card1).unbind();
-        $(cards.card2).unbind();
-        cards.reset();
+    if ($(cards.card1.getElementsByClassName('back')[0]).css('background-image') == $(cards.card2.getElementsByClassName('back')[0]).css('background-image')){
+        game.match();
     }
     else{
-        setTimeout(() => {
-            $(cards.card1).css('background-image' , '');
-            $(cards.card2).css('background-image' , '');
-            console.log('FillipBack();');
-            cards.reset();
-        }, 2000);
+        game.fillipBack();
     }
 };
 
+game.match=()=>{
+    $(cards.card1).unbind();
+    $(cards.card2).unbind();
+    cards.reset();
+    game.giveScore();
+}
+game.fillipBack = () =>{
+    setTimeout(() => {
+        $(cards.card1).removeClass('is-flipped');
+        $(cards.card2).removeClass('is-flipped');
+        cards.reset();
+    }, 2000);
+}
 
+game.giveScore = ()=>{
+    game.score++;
+    console.log(game.score);
+    if (game.score == imgBase.numOfImagesToGet){
+        console.log('win');
+    }
+    
+}
 
-game.start();
 
 
 
