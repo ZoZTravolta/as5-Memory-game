0fddd57d487b6d9909f255440484271f68abfcf7
Zohar <Zoharsaghi@gmail.com>
2019-11-30 11:13:19 +0200

    all is working but reset button

--- js/script.js
+++ js/script.js
@@ -1,16 +1,32 @@
 'use strict';
-
-
-
 $(document).ready(function(){
-    game.getInputFromUsers();
-})
-const imgBase = {};
-const cards = {};
-const game = {};
-const modal = {};
+    game.reset();
+    $('.reset-btn').on('click', function(){
+        game.reset();
+    })
+});
 
-game.getInputFromUsers=()=>{
+
+
+let imgBase = {};
+let cards = {};
+let game = {};
+let modal = {};
+
+
+
+game.reset=()=>{
+    $('.welcome').show();
+    $('#theme-buttons button').removeClass('selected');
+    $('#level-buttons button').removeClass('selected');
+   
+    $('.game').empty();
+    $('.loader').addClass('show');
+    imgBase.imgArr = [];  
+     game.getInputsFromUser();
+};
+
+game.getInputsFromUser=()=>{
     $('#theme-buttons button').on('click', function(){
         imgBase.numOfImagesToGet = this.value;
         $('#theme-buttons button').removeClass('selected')
@@ -28,15 +44,14 @@ game.getInputFromUsers=()=>{
 game.checkIfWeCanStart=()=>{
     if ($('#level-buttons button').hasClass('selected') &&  $('#theme-buttons button').hasClass('selected')){
         let theme = $('#theme-buttons button.selected').val();
-        console.log(theme)
         let numOfCards = $('#level-buttons button.selected').val();
         game.start(theme , numOfCards);
         $('.welcome').hide();
     }
-}
+};
 
 game.start=(theme,numOfCards)=>{
-    
+    $('body').removeClass();
     switch (theme){
         case 'dogs':
             imgBase.imgApi = 'https://dog.ceo/api/breeds/image/random';
@@ -51,16 +66,18 @@ game.start=(theme,numOfCards)=>{
     }
     
     imgBase.numOfImagesToGet = numOfCards;
-    imgBase.imgArr = [];
     imgBase.buildPicsDataBase();
     cards.reset();
     game.score = 0;
-}
+};
+
 
 
 imgBase.buildPicsDataBase = async () => {
-    $('.loader').addClass('show')
-    for (let i = 0; i < imgBase.numOfImagesToGet; i++) {
+   let numOfImagesToGet = imgBase.numOfImagesToGet;
+   let imgArr;
+    $('.loader').addClass('show');
+    for (let i = 0; i < numOfImagesToGet; i++) {
         try {
             let getImage = await $.get({
                 url: imgBase.imgApi,
@@ -73,20 +90,22 @@ imgBase.buildPicsDataBase = async () => {
             console.log('error loading images')
         }
     }
+    
+    
     imgBase.shuffleArray();
     imgBase.loadAllImages();
     $('.loader').removeClass('show');
 
-}
+};
 
 imgBase.loadAllImages =() =>{
     for(let img of imgBase.imgArr){
         $('.img-tank').append(`<img src="${img}" />`)
     }
-    
-}
+};
 
 imgBase.shuffleArray = () => {
+    
     function shuffle(arr) {
         let ctr = arr.length;
         let temp;
@@ -106,15 +125,16 @@ imgBase.shuffleArray = () => {
     }
     shuffle(imgBase.imgArr)
     cards.createCards();
-}
+};
 
 cards.reset=()=>{
     cards.card1 = null;
     cards.card2 = null;
     $('.memoryCard').attr("disabled", false);
-}
+};
 
 cards.createCards = () => {
+    
     for (let index in imgBase.imgArr) {
         $('.game').append(`<div class="memoryCard" id="${index}" value="${index}">
             <div class="front"></div>
@@ -135,7 +155,7 @@ cards.clickOnCard = (cardThatClicked) => {
         console.log('disabled')
     }
     
-}
+};
 
 cards.flipCard = (cardThatClicked) =>{
     const back = cardThatClicked.getElementsByClassName('back')[0];
@@ -150,7 +170,7 @@ cards.flipCard = (cardThatClicked) =>{
         back.style.backgroundImage = `url("${imgBase.imgArr[cardThatClicked.id]}")`;
         cards.checkIfMatch()
     }
-}
+};
 
 cards.checkIfMatch =()=>{
     if ($(cards.card1.getElementsByClassName('back')[0]).css('background-image') == $(cards.card2.getElementsByClassName('back')[0]).css('background-image')){
@@ -166,14 +186,14 @@ game.match=()=>{
     $(cards.card2).unbind();
     cards.reset();
     game.giveScore();
-}
+};
 game.fillipBack = () =>{
     setTimeout(() => {
         $(cards.card1).removeClass('is-flipped');
         $(cards.card2).removeClass('is-flipped');
         cards.reset();
     }, 1000);
-}
+};
 
 game.giveScore = ()=>{
     game.score++;
@@ -182,7 +202,7 @@ game.giveScore = ()=>{
         console.log('win');
     }
     
-}
+};
 
 
 
